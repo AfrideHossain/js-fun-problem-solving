13858f309b99cc96103ac2897ca5023bd5e3d3f2
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2023-07-27 17:28:39 +0600

    romanToInt modified

--- romanToInt.js
+++ romanToInt.js
@@ -5,23 +5,26 @@ function romanToInt(roman = "") {
     v: 5,
     x: 10,
     l: 50,
+    c: 100,
     d: 500,
     m: 1000,
   };
   // transform to lowercase and reverse the string
   let romanStr = roman.toLowerCase().split("").reverse().join("");
   let decimal = 0; //decimal number;
-  for (let char of romanStr) {
-    let romanVal = romanMap[char];
-    console.log(romanVal);
-    if (decimal >= romanVal) {
-      decimal = decimal - romanVal;
+  for (let i = 0; i < romanStr.length; i++) {
+    let previousRomanChar = romanStr[i - 1];
+    let currentRomanChar = romanStr[i];
+    if (romanMap[currentRomanChar] < romanMap[previousRomanChar]) {
+      decimal -= romanMap[currentRomanChar];
     } else {
-      decimal += romanVal;
+      decimal += romanMap[currentRomanChar];
     }
   }
-  console.log(decimal);
+  return decimal;
 }
 
 //testing function
-romanToInt("iii");
+let romanString = "mcmxciv";
+let result = romanToInt(romanString);
+console.log(result);
